2574d048f711ad1ceeb61bc0a83d37d03f87e7cd
TiagoFP <tiagopimenteldev@gmail.com>
2023-05-19 19:28:13 -0300

    Final Commit

--- src/desafiodio/Main.java
+++ src/desafiodio/Main.java
@@ -22,8 +22,38 @@ public class Main {
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+        //System.out.println(curso1);
+        //System.out.println(curso2);
+        //System.out.println(mentoria);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição do Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devTiago = new Dev();
+        devTiago.setNome("Tiago");
+        devTiago.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos" + devTiago.getConteudosInscritos());
+        devTiago.progredir();
+
+        System.out.println("Conteudos Inscritos" + devTiago.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos" + devTiago.getConteudosConcluidos());
+        System.out.println("XP: " + devTiago.calcularTotalXp());
+        System.out.println("---");
+
+        Dev devHeitor = new Dev();
+        devHeitor.setNome("Heitor");
+        devHeitor.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos" + devHeitor.getConteudosInscritos());
+        devHeitor.progredir();
+
+        System.out.println("-- -");
+
+        System.out.println("Conteudos Inscritos" + devHeitor.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos" + devHeitor.getConteudosConcluidos());
+        System.out.println("XP: " + devHeitor.calcularTotalXp());
     }
 }
